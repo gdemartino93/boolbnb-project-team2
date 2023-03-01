68ae98805eec679bbe1b8f4f8a7d6a040fb39bdb
esposito.00@outlook.it <esposito.00@outlook.it>
2023-03-01 12:19:23 +0100

    added messages table

---
+++ database/migrations/2023_03_01_111358_create_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('text')->required();
+            $table->string('mail', 128)->required()->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
