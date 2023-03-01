ee46527ff42c4ac699082f042ca2531a3f490c94
19nikilangiu91 <niki.langiu@gmail.com>
2023-03-01 11:35:34 +0100

    edit apartments t able

---
+++ database/migrations/2023_03_01_101626_create_apartments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->tinyInteger('room_number')->unsigned();
+            $table->tinyInteger('bed_number')->unsigned();
+            $table->tinyInteger('bath_number')->unsigned();
+            $table->tinyInteger('square_meters')->unsigned();
+            $table->string('address', 81);
+            $table->string('img', 64);
+            $table->boolean('visible')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
