e940d81ad00e46a8320eacc552244ed081bb38b7
19nikilangiu91 <niki.langiu@gmail.com>
2023-03-02 17:18:51 +0100

    “View-Relation”

--- app/Models/View.php
+++ app/Models/View.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class View extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'ip',
+        'date',
+    ];
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
