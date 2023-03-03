f4eae4d515522fc5042ed463099f479813c7b6b8
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-03 11:07:26 +0100

    fixed AdditionalService factory name

--- database/factories/AdditionaLServiceFactory.php
+++ database/factories/AdditionaLServiceFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AdditionaLService>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AdditionalService>
  */
-class AdditionaLServiceFactory extends Factory
+class AdditionalServiceFactory extends Factory
 {
     /**
      * Define the model's default state.
