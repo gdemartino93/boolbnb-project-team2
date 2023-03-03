d076589469e4ef8057b6bce5b83672dbc3b3e0e5
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-03 11:56:17 +0100

    created Sponsorship & AService seeders

---
+++ database/seeders/AdditionalServiceSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\AdditionalService;
+
+class AdditionalServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        AdditionalService::factory() -> count(50) -> create();
+    }
+}
