239fac50df5f78880c343847d2386e3a5bd77076
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-04 00:21:26 +0100

    Register page

--- vue-frontend/src/components/Nav.vue
+++ vue-frontend/src/components/Nav.vue
@@ -3,6 +3,7 @@
         <ul>
             <RouterLink :to="{name: 'home'}">Home</RouterLink>
             <RouterLink :to="{name: 'login'}">Login</RouterLink>
+            <RouterLink :to="{name: 'register'}">Register</RouterLink>
 
             <!-- <router-link :to="{name : "HomeX"}">ASDXASD</router-link> -->
 
--- vue-frontend/src/components/Register.vue
+++ vue-frontend/src/components/Register.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        <h1>REGISTRAZIONE PAGINA</h1>
+        <h1>login</h1>
+        <form class="d-flex">
+            <label for="name">Nome</label>
+            <input type="text" name="name" >
+            <label for="surname">Cognome</label>
+            <input type="text" name="surname" >
+            <label for="date">Data di nascita</label>
+            <input type="date" name="date" >
+            <label for="email">Email</label>
+            <input type="text" name="email" >
+            <label for="email">Password</label>
+            <input type="password" name="password" >
+            <label for="email">Repeat Password</label>
+            <input type="password" name="password" >
+            <button type="submit" class="">Login</button>
+        </form>
     </div>
 </template>
 
@@ -9,5 +24,10 @@
 </script>
 
 <style lang="scss" scoped>
-
+.d-flex{
+    display: flex;
+    flex-direction: column;
+    width: 500px;
+    margin: 0 auto;
+}
 </style>
