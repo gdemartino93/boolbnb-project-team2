354cc3155c5980817c3ea06bbae44afbe7e3afc0
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-03 10:43:45 +0100

    created Sponsorship factory

---
+++ database/factories/SponsorshipFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sponsorship>
+ */
+class SponsorshipFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            
+            'type' => fake() -> lexify('sponsor-????'),
+            'price' => fake() -> randomFloat(2, 2, 9)
+        ];
+    }
+}
