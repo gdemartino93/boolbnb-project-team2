c4d69b38cf835d0cea8603c74e8e25df8e2a2eee
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-04 11:46:37 +0100

    created store

--- vue-frontend/src/components/Nav.vue
+++ vue-frontend/src/components/Nav.vue
@@ -4,15 +4,22 @@
             <RouterLink :to="{name: 'home'}">Home</RouterLink>
             <RouterLink :to="{name: 'login'}">Login</RouterLink>
             <RouterLink :to="{name: 'register'}">Register</RouterLink>
-
+           
             <!-- <router-link :to="{name : "HomeX"}">ASDXASD</router-link> -->
-
+            logout
         </ul>
     </div>
 </template>
 
-<script setup>
-
+<script>
+import { store } from '../stores/store';
+export default{
+    data(){
+        return{
+            store
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
