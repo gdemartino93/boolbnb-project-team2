57600752365faec96632da29617094042123dab0
19nikilangiu91 <niki.langiu@gmail.com>
2023-03-06 16:48:29 +0100

    “Fixed-Form”

--- vue-frontend/src/components/auth/Dashboard.vue
+++ vue-frontend/src/components/auth/Dashboard.vue
@@ -1,16 +1,16 @@
 <script>
 import { useAuthStore } from '../../stores/auth';
 
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
 
-            authStore : useAuthStore(),
+            authStore: useAuthStore(),
         }
     },
     methods: {
     },
-    mounted(){
+    mounted() {
 
         this.authStore.getUsersWithApt();
     }
@@ -18,60 +18,66 @@ export default{
 </script>
 
 <template>
-    <RouterLink :to="{ name: 'addApt' }">
-        <a href="#">Aggiungi appartamento</a>
-        <br>
-        <form action="">
-            <label for="title">title</label>
-            <input type="text" name="title">
-            <br>
+    <!-- <RouterLink :to="{ name: 'addApt' }"> -->
+    <a href="#">Aggiungi appartamento</a>
 
-    <h2>CIAO SEI NELLA DASHBOARD {{$route.params.id}}</h2>
+    <h2>CIAO SEI NELLA DASHBOARD {{ $route.params.id }}</h2>
 
     <h3>List of apartments</h3>
 
     <ul v-if="authStore.apartments">
-    <li v-for="apartment in authStore.apartments"> {{ apartment.title }}</li>
-    </ul> 
+        <li v-for="apartment in authStore.apartments"> {{ apartment.title }}</li>
+    </ul>
 
-            <label for="room_number">room number</label>
-            <input type="number" name="room_number">
-            <br>
+    <h2>Form</h2>
+    <form action="">
+        <label for="title">Title</label>
+        <input type="text" name="title">
+        <br>
 
-            <label for="bed_number">bed number</label>
-            <input type="number" name="bed_number">
-            <br>
+        <label for="description">Description</label>
+        <input type="text" name="description">
+        <br>
 
-            <label for="bath_number">bath number</label>
-            <input type="number" name="bath_number">
-            <br>
+        <label for="room_number">Room Number</label>
+        <input type="number" name="room_number">
+        <br>
 
-            <label for="square_meters">square meters</label>
-            <input type="number" name="square_meters">
-            <br>
+        <label for="bed_number">Bed Number</label>
+        <input type="number" name="bed_number">
+        <br>
 
-            <label for="address">address</label>
-            <input type="text" name="address">
-            <br>
+        <label for="bath_number">Bath Number</label>
+        <input type="number" name="bath_number">
+        <br>
 
-            <label for="latitude">latitude</label>
-            <input type="number" name="latitude">
-            <br>
+        <label for="square_meters">Square Meters</label>
+        <input type="number" name="square_meters">
+        <br>
 
-            <label for="longitude">longitude</label>
-            <input type="number" name="longitude">
-            <br>
+        <label for="address">Address</label>
+        <input type="text" name="address">
+        <br>
 
-            <label for="img">img</label>
-            <input type="url" name="img">
-            <br>
+        <label for="latitude">Latitude</label>
+        <input type="number" name="latitude">
+        <br>
 
-            <label for="additional_services_id">additional services id</label>
-            <input type="text" name="additional_services_id">
-            <br>
-        </form>
+        <label for="longitude">Longitude</label>
+        <input type="number" name="longitude">
+        <br>
 
-    </RouterLink>
+        <label for="img">Image</label>
+        <input type="url" name="img">
+        <br>
+
+        <label for="additional_services">Additional Services</label>
+        <input type="text" name="additional_services">
+        <br>
+        <input @click="submit" type="submit" value="Create New Apartment">
+    </form>
+
+    <!-- </RouterLink> -->
 </template>
 
 <style scoped></style>
