f91c61b1143e8bb87470fde34620e79ad0424b17
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-06 23:32:52 +0100

    created store

--- routes/api.php
+++ routes/api.php
@@ -16,19 +16,23 @@ use App\Http\Controllers\ApiController;
 |
 */
 
+Route::middleware('auth:sanctum') ->group(function(){
+
+  
+
+
+
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
 
+});
+Route :: post('/v1/apartment/store', [ApiController :: class, 'store']);
 Route::middleware('auth:sanctum')->get('/user/apartments', [ApiController::class, 'dashboardList']);
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::get('/v1/apartment/all', [ApiController::class, 'index']);
 
-Route :: post('/v1/apartment/store', [ApiController :: class, 'store']);
 
 Route :: post('/v1/apartment/update/{apartment}', [ApiController :: class, 'update']);
 
--- vue-frontend/src/components/auth/AddApt.vue
+++ vue-frontend/src/components/auth/AddApt.vue
@@ -1,56 +1,95 @@
 <script>
+import { ref } from 'vue';
+import {store} from '../../stores/store'
+import { useAuthStore } from '../../stores/auth';
+import axios from 'axios';
 export default {
+    data(){
+        return{
+            auth : useAuthStore(),
+            store,
+            // inserimento nuovo appartamento
+            form: ref({
+                title: "",
+                description:"",
+                room_number: undefined,
+                bed_number:undefined,
+                bath_number:undefined,
+                square_meters:undefined,
+                address:"",
+                latitude: undefined,
+                longitude:undefined,
+                img :"",
+                additional_services: []
+            }),
+
+        }
+    },
+    methods:{
+        async storeData(e){
+            e.preventDefault();
+            await this.auth.getToken();
+           try {
+            await axios.post('/api/v1/apartment/store',this.form)
+           } catch (error) {
+            console.log(error)
+           }
+        }
+    }
 
 }
 </script>
 
 <template>
     <h2>Form</h2>
-    <form action="">
+    <form action="" method="POST">
         <label for="title">Title</label>
-        <input type="text" name="title">
+        <input type="text" name="title" v-model="form.title">
         <br>
 
         <label for="description">Description</label>
-        <input type="text" name="description">
+        <input type="text" name="description" v-model="form.description">
         <br>
 
         <label for="room_number">Room Number</label>
-        <input type="number" name="room_number">
+        <input type="number" name="room_number" v-model="form.room_number">
         <br>
 
         <label for="bed_number">Bed Number</label>
-        <input type="number" name="bed_number">
+        <input type="number" name="bed_number" v-model="form.bed_number">
         <br>
 
         <label for="bath_number">Bath Number</label>
-        <input type="number" name="bath_number">
+        <input type="number" name="bath_number" v-model="form.bath_number">
         <br>
 
         <label for="square_meters">Square Meters</label>
-        <input type="number" name="square_meters">
+        <input type="number" name="square_meters" v-model="form.square_meters">
         <br>
 
         <label for="address">Address</label>
-        <input type="text" name="address">
+        <input type="text" name="address" v-model="form.address">
         <br>
 
         <label for="latitude">Latitude</label>
-        <input type="number" name="latitude">
+        <input type="number" name="latitude" v-model="form.latitude">
         <br>
 
         <label for="longitude">Longitude</label>
-        <input type="number" name="longitude">
+        <input type="number" name="longitude" v-model="form.longitude">
         <br>
 
-        <label for="img">Image</label>
-        <input type="url" name="img">
+        <label for="img" >Image</label>
+        <input type="text" name="img" v-model="form.img">
         <br>
 
-        <label for="additional_services">Additional Services</label>
-        <input type="text" name="additional_services">
+        <div v-for="service in store.additional_services" :key="service.id">
+            <label :for="service.name"> {{ service.name }} </label>
+            <input type="checkbox" :name="service.name" :id="service.id" v-model="form.additional_services">
+        </div>
+
         <br>
-        <input @click="submit" type="submit" value="Create New Apartment">
+        <input @click="storeData" type="submit" value="Create New Apartment">
     </form>
 </template>
 
