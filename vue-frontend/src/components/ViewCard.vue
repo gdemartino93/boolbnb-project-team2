4dde9baf0b269c0a32e53d4b19b0363ea4cc6ffd
19nikilangiu91 <niki.langiu@gmail.com>
2023-03-07 19:04:24 +0100

    “add-content-view-card”

--- vue-frontend/src/components/ViewCard.vue
+++ vue-frontend/src/components/ViewCard.vue
@@ -1,30 +1,30 @@
 <script>
-import {useAuthStore} from '../stores/auth';
+import { useAuthStore } from '../stores/auth';
 import axios from 'axios';
 export default {
-    data(){
-        return{
+    data() {
+        return {
 
             authStore: useAuthStore(),
-            apt : {},
-            services : []
+            apt: {},
+            services: []
         }
     },
-    methods:{
-        async getSingleAp(id){
+    methods: {
+        async getSingleAp(id) {
             try {
-               const response =  await axios.get('/api/v1/apartment/' + id )
-               this.apt = response.data.response;
-                this.services = response.data.additional_services.response;
-               
-                
+                const response = await axios.get('/api/v1/apartment/' + id)
+                this.apt = response.data.response;
+                this.services = response.data.response.additional_services;
+
+
             } catch (error) {
                 console.log(error)
             }
-         
+
         }
     },
-    mounted(){
+    mounted() {
         this.getSingleAp(this.$route.params.id);
         this.authStore.getUsersWithApt();
     }
@@ -32,20 +32,25 @@ export default {
 </script>
 
 <template>
-    <!-- <div v-for="{id,title} in apts" >
-      <div>ID: {{ id }}, Nome: {{ title }}</div>
-    </div> -->
-
-    <div>
-        ID: {{ apt.id }}
-        name: {{ apt.title }}
-    </div>
-    <!-- <div>
-      <ul>lista add services
-        <li v-for="service in services">{{ service.name }}</li>
-      </ul>
-    </div> -->
-  </template>
+    <ul>
+        <li>Id: {{ apt.id }}</li>
+        <li><img :src="apt.img" :alt="apt.img"></li>
+        <li>Title: {{ apt.title }}</li>
+        <li>Description: {{ apt.description }}</li>
+        <li>Room Number: {{ apt.room_number }}</li>
+        <li>Bed Number: {{ apt.bed_number }}</li>
+        <li>Bath Number: {{ apt.bath_number }}</li>
+        <li>Square Meters: {{ apt.square_meters }}</li>
+        <li>Address: {{ apt.address }}</li>
+        <li>Latitude: {{ apt.latitude }}</li>
+        <li>Longitude: {{ apt.longitude }}</li>
+    </ul>
+    <h6>Additional Services</h6>
+    <ul v-for="service in services">
+        <li>Name: {{ service.name }}</li>
+        <li>Description: {{ service.description }}</li>
+    </ul>
+</template>
   
 
 <style scoped></style>
