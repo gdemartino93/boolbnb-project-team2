a89c70777ade66def253436384f3abd98468f72a
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-08 10:44:44 +0100

    validation name and lastname

--- vue-frontend/src/components/auth/Register.vue
+++ vue-frontend/src/components/auth/Register.vue
@@ -9,6 +9,7 @@ import { useAuthStore } from '../../stores/auth';
         authStore: useAuthStore(),
         form: ref({
           name: "",
+          errName: false,
           lastname: "",
           birthdate: "",
           email: "",
@@ -18,9 +19,18 @@ import { useAuthStore } from '../../stores/auth';
       };
     },
     methods:{
-      // goTo(){
-        
-      // }
+checkAge() {
+    const birthdate = new Date(this.form.birthdate);
+    const today = new Date();
+    const age = today.getFullYear() - birthdate.getFullYear();
+    const monthDiff = today.getMonth() - birthdate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdate.getDate())) {
+      age--;
+    }
+    return age >= 18;
+  }
+
+
     }
   };
   </script>
@@ -34,9 +44,9 @@ import { useAuthStore } from '../../stores/auth';
       <!-- <div class="d-flex flex-column"> -->
         <!-- Step 1 -->
         <div class="step d-flex justify-content-center align-items-center">
-          <label for="name">Nome</label>
+          <label for="name">Inserisci il tuo nome</label>
           <input type="text" name="name" v-model="form.name">
-          <button class="btn btn-success mx-2" type="button" @click="this.$refs.lastname.scrollIntoView({behavior: 'smooth'})">next</button>
+          <button class="btn btn-success mx-2" type="button" v-if="form.name.length > 0" @click="this.$refs.lastname.scrollIntoView({behavior: 'smooth'})">next</button>
           <!-- Gestione errore -->
           <div v-if="authStore.errors.name">
             <span class="text-danger">{{ authStore.errors.name[0] }}</span>
@@ -44,9 +54,9 @@ import { useAuthStore } from '../../stores/auth';
         </div>
         <!-- Step 2 -->
         <div class="step d-flex justify-content-center align-items-center" >
-          <label for="lastname">Cognome</label>
+          <label for="lastname">Inserisci il tuo cognome</label>
           <input type="text" name="lastname" v-model="form.lastname" ref="lastname">
-          <button class="btn btn-success mx-2" type="button" @click="this.$refs.dateOB.scrollIntoView({behavior: 'smooth'})">next</button>
+          <button class="btn btn-success mx-2" type="button" v-if="form.lastname.length > 0" @click="this.$refs.dateOB.scrollIntoView({behavior: 'smooth'})">next</button>
           <!-- Gestione errore -->
           <div v-if="authStore.errors.lastname">
             <span class="text-danger">{{ authStore.errors.lastname[0] }}</span>
@@ -54,9 +64,9 @@ import { useAuthStore } from '../../stores/auth';
         </div>
         <!-- Step 3 -->
         <div class="step d-flex justify-content-center align-items-center">
-          <label for="birthdate">Data di nascita</label>
+          <label for="birthdate">Inserisci la tua Data di Nascita, devi avere più di 18 anni</label>
           <input type="date" name="birthdate" v-model="form.birthdate" ref="dateOB">
-          <button class="btn btn-success mx-2" type="button" @click="this.$refs.email.scrollIntoView({behavior: 'smooth'})">next</button>
+          <button class="btn btn-success mx-2" type="button"  @click="this.$refs.email.scrollIntoView({behavior: 'smooth'})">next</button>
           <!-- Gestione errore -->
           <div v-if="authStore.errors.birthdate">
             <span class="text-danger">{{ authStore.errors.birthdate[0] }}</span>
