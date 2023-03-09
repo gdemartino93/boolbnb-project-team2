de5c4a62c0082bf88f2dfadf759e3e698414ff5e
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-09 12:56:37 +0100

    working on edit form

--- vue-frontend/src/components/auth/EditApt.vue
+++ vue-frontend/src/components/auth/EditApt.vue
@@ -11,30 +11,31 @@ export default {
             store,
             apt: {},
             services: [],
+            adds: [],
             // inserimento nuovo appartamento
-            // form: ref({
-            //     title: "",
-            //     description: "",
-            //     room_number: undefined,
-            //     bed_number: undefined,
-            //     bath_number: undefined,
-            //     square_meters: undefined,
-            //     address: "",
-            //     latitude: undefined,
-            //     longitude: undefined,
-            //     img: "",
-            //     additional_services: []
-            // }),
+            form: ref({
+                title: "",
+                description: "",
+                room_number: undefined,
+                bed_number: undefined,
+                bath_number: undefined,
+                square_meters: undefined,
+                address: "",
+                latitude: undefined,
+                longitude: undefined,
+                img: "",
+                additional_services: []
+            }),
 
         }
     },
     methods: {
-        async storeData(e) {
-            e.preventDefault();
+        async updateData(apartment) {
+            // e.preventDefault();
             await this.auth.getToken();
             try {
 
-                await axios.post('/api/v1/apartment/store', this.form)
+                await axios.post('/api/v1/apartment/update/' + apartment, this.form)
             } catch (error) {
 
                 console.log(error)
@@ -46,8 +47,8 @@ export default {
             try {
 
                 const response = await axios.get('api/v1/services/all');
-                this.services = response.data.response;
-                console.log(this.services);
+                this.adds = response.data.response;
+                console.log(this.adds);
             } catch (error) {
 
                 console.log(error);
@@ -57,7 +58,8 @@ export default {
             try {
                 const response = await axios.get('/api/v1/apartment/' + id)
                 this.apt = response.data.response;
-                this.services = response.data.response.additional_services;
+                // this.services = response.data.response.additional_services;
+                // this.services = response.data.response;
 
 
             } catch (error) {
@@ -65,10 +67,25 @@ export default {
             }
 
         },
+        checkCheckboxes(apartment, additional_service){
+
+            for(let x = 0; x < apartment.additional_service.length; x++ ){
+
+                const apartmentAdds = apartment.additional_service[x];
+
+                if(apartmentAdds.id == additional_service.id){
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
     },
     mounted() {
         this.getSingleAp(this.$route.params.id);
         this.getServices();
+        this.checkCheckboxes();
     }
 
 }
@@ -78,49 +95,53 @@ export default {
     <h2>Form</h2>
     <form action="" method="POST">
         <label for="title">Title</label>
-        <input type="text" name="title" :value="apt.title">
+        <input type="text" name="title" :value="apt.title" v-model="form.title">
         <br>
 
         <label for="description">Description</label>
-        <input type="text" name="description" :value="apt.description">
+        <input type="text" name="description" :value="apt.description" v-model="form.description">
         <br>
 
         <label for="room_number">Room Number</label>
-        <input type="number" name="room_number" :value="apt.room_number">
+        <input type="number" name="room_number" :value="apt.room_number" v-model="form.room_number">
         <br>
 
         <label for="bed_number">Bed Number</label>
-        <input type="number" name="bed_number" :value="apt.bed_number">
+        <input type="number" name="bed_number" :value="apt.bed_number" v-model="form.bed_number">
         <br>
 
         <label for="bath_number">Bath Number</label>
-        <input type="number" name="bath_number" :value="apt.bath_number">
+        <input type="number" name="bath_number" :value="apt.bath_number" v-model="form.bath_number">
         <br>
 
         <label for="square_meters">Square Meters</label>
-        <input type="number" name="square_meters" :value="apt.square_meters">
+        <input type="number" name="square_meters" :value="apt.square_meters" v-model="form.square_meters">
         <br>
 
         <label for="address">Address</label>
-        <input type="text" name="address" :value="apt.address">
+        <input type="text" name="address" :value="apt.address" v-model="form.address">
         <br>
 
         <label for="latitude">Latitude</label>
-        <input type="number" name="latitude" :value="apt.latitude">
+        <input type="number" name="latitude" :value="apt.latitude" v-model="form.latitude">
         <br>
 
         <label for="longitude">Longitude</label>
-        <input type="number" name="longitude" :value="apt.longitude">
+        <input type="number" name="longitude" :value="apt.longitude" v-model="form.longitude">
         <br>
 
         <label for="img">Image</label>
-        <input type="text" name="img" :value="apt.img">
+        <input type="text" name="img" :value="apt.img" v-model="form.img">
         <br>
-        <!-- <p>{{ apt.additional_services }}</p> -->
-        <div v-for="(service, index) in services.additional_service" :key="service.id">
-
-            <input type="checkbox" :id="service.id" :value="service.id" v-model="services.additional_services">
-
+        
+        <!-- <p>{{ adds.additional_service }}</p>
+        <p>{{ apt }}</p> -->
+        
+        <div v-for="service in adds.additional_service" :key="service.id">
+            <!-- <p>{{ service }}</p> -->
+            <input type="checkbox" :id="service.id" :value="service.id" v-model="form.additional_services">
+            <!-- :checked="apt.additional_services.pivot.additional_service_id === service.id ? 'true' : 'false'  -->
+            <!-- :checked="apt.additional_services.includes(service) ? 'true' : 'false'" -->
             <label :for="service.name"> {{ service.name }} </label>
 
         </div>
@@ -128,7 +149,7 @@ export default {
         <br>
 
         <!-- <router-link :to="{name: 'home'}" > -->
-        <input @click="" type="submit" value="Update Apartment">
+        <input @click="updateData(apt.id)" type="submit" value="Update Apartment">
         <!-- </router-link> -->
     </form>
 </template>
