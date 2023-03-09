1df89e954f8793d2aa12413d0b144ba5bdbb105a
Salvatore Esposito <esposito.00@outlook.it>
2023-03-09 11:15:10 +0100

    populated col apartment_sponsorship in db

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -31,7 +31,7 @@ class ApartmentSeeder extends Seeder
             $sponsorships->each(function ($sponsorship) use ($apartment) {
             // l'attach passa una data formato timestamp quindi per passarla correttamente nel db dobbiamo convertirla in formato data che si aspetta la colonna
             // $expiringDate = Carbon::now()->addDays(rand(-180, 0))->format('Y-m-d');
-            // $apartment->sponsorships()->attach($sponsorship, ['expiring_date' => $expiringDate]);
+            $apartment->sponsorships()->attach($sponsorship);
             });
         });
     }
