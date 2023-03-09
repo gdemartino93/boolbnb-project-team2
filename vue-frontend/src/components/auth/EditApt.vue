f180406726766b93912c0136fa760c5b42616258
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-10 00:29:31 +0100

    fix edit form

--- vue-frontend/src/components/auth/EditApt.vue
+++ vue-frontend/src/components/auth/EditApt.vue
@@ -1,137 +1,126 @@
 <script>
 import { ref } from 'vue';
-import { store } from '../../stores/store'
+import { store } from '../../stores/store';
 import { useAuthStore } from '../../stores/auth';
 import axios from 'axios';
 
 export default {
-    data() {
-        return {
-            auth: useAuthStore(),
-            store,
-            apt: {},
-            services: [],
-            adds: []
-
-        }
-    },
-    methods: {
-        async updateData(apartment) {
-            await this.auth.getToken();
-            try {
-
-                await axios.post('/api/v1/apartment/update/' + apartment.id, this.apt)
-            } catch (error) {
-
-                console.log(error)
-            }
-
-            this.$router.push('/');
-        },
-        async getServices() {
-            try {
-
-                const response = await axios.get('api/v1/services/all');
-                this.adds = response.data.response;
-            } catch (error) {
-
-                console.log(error);
-            }
-        },
-        async getSingleAp(id) {
-            try {
-                const response = await axios.get('/api/v1/apartment/' + id)
-                this.apt = response.data.response;
-
-            } catch (error) {
-                console.log(error)
-            }
-
-        },
-        checkCheckboxes(apartment, addService){
-
-            // Ciclo for che verifica ad ogni iterazione se l'elemento x dell'array additional_services in apartment ha id uguale a quello del singolo servizio, sono state usate due variabili generiche apartment e addService 
-            for(let x = 0; x < apartment.additional_services.length; x++ ){
-
-                const apartmentAdds = apartment.additional_services[x];
-
-                if(apartmentAdds.id == addService.id){
-
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    },
-    mounted() {
-        this.getSingleAp(this.$route.params.id);
-        this.getServices();
-        
+  data() {
+    return {
+      auth: useAuthStore(),
+      store,
+      apt: {},
+      adservices: [],
+      services: [],
+      adds: [],
     }
-
+  },
+  methods: {
+    async updateData(apartment) {
+      await this.auth.getToken();
+      try {
+        await axios.post('/api/v1/apartment/update/' + this.apt.id, this.apt);
+      } catch (error) {
+        console.log(error);
+      }
+      this.$router.push('/');
+    },
+    async getServices() {
+      try {
+        const response = await axios.get('api/v1/services/all');
+        this.adds = response.data.response;
+        ser = response.data.response;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getSingleAp(id) {
+      try {
+        const response = await axios.get('/api/v1/apartment/' + id);
+        this.apt = response.data.response;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    getAdServices(value) {
+      this.adservices.push(value);
+    },
+    checkCheckboxes(apartment, addService) {
+      for (let x = 0; x < apartment.additional_services.length; x++) {
+        const apartmentAdds = apartment.additional_services[x];
+        this.adservices.push(apartment.additional_services[x])
+        if (apartmentAdds.id == addService.id) {
+          return true;
+        }
+      }
+      return false;
+    },
+  },
+  mounted() {
+    this.getSingleAp(this.$route.params.id);
+    this.getServices();
+  },
 }
 </script>
 
-<template>  
+<template>
     <section v-if="auth.user">
-        <h2>Form</h2>
-        <form action="" method="POST">
-            <label for="title">Title</label>
-            <input type="text" name="title" :value="apt.title">
-            <br>
-
-            <label for="description">Description</label>
-            <input type="text" name="description" :value="apt.description">
-            <br>
-
-            <label for="room_number">Room Number</label>
-            <input type="number" name="room_number" :value="apt.room_number">
-            <br>
-
-            <label for="bed_number">Bed Number</label>
-            <input type="number" name="bed_number" :value="apt.bed_number">
-            <br>
-
-            <label for="bath_number">Bath Number</label>
-            <input type="number" name="bath_number" :value="apt.bath_number" >
-            <br>
-
-            <label for="square_meters">Square Meters</label>
-            <input type="number" name="square_meters" :value="apt.square_meters" >
-            <br>
-
-            <label for="address">Address</label>
-            <input type="text" name="address" :value="apt.address" >
-            <br>
-
-            <label for="latitude">Latitude</label>
-            <input type="number" name="latitude" :value="apt.latitude" >
-            <br>
-
-            <label for="longitude">Longitude</label>
-            <input type="number" name="longitude" :value="apt.longitude" >
-            <br>
-
-            <label for="img">Image</label>
-            <input type="text" name="img" :value="apt.img" >
-            <br>        
-            
-            <div v-for="service in adds.additional_service" :key="service.id">
-                <input type="checkbox" :id="service.id" :value="service.id" :checked="checkCheckboxes(apt, service)">
-                <label :for="service.name"> {{ service.name }} </label>
-            </div>
-
-            <br>
-
-            <input @click.prevent="updateData(apt)" type="submit" value="Update Apartment">
-        </form>
+      <h2>Form</h2>
+      <form action="" method="POST">
+        <label for="title">Title</label>
+        <input type="text" name="title" v-model="apt.title">
+        <br>
+  
+        <label for="description">Description</label>
+        <input type="text" name="description" v-model="apt.description">
+        <br>
+  
+        <label for="room_number">Room Number</label>
+        <input type="number" name="room_number" v-model="apt.room_number">
+        <br>
+  
+        <label for="bed_number">Bed Number</label>
+        <input type="number" name="bed_number" v-model="apt.bed_number">
+        <br>
+  
+        <label for="bath_number">Bath Number</label>
+        <input type="number" name="bath_number" v-model="apt.bath_number">
+        <br>
+  
+        <label for="square_meters">Square Meters</label>
+        <input type="number" name="square_meters" v-model="apt.square_meters">
+        <br>
+  
+        <label for="address">Address</label>
+        <input type="text" name="address" v-model="apt.address">
+        <br>
+  
+        <label for="latitude">Latitude</label>
+        <input type="number" name="latitude" v-model="apt.latitude">
+        <br>
+  
+        <label for="longitude">Longitude</label>
+        <input type="number" name="longitude" v-model="apt.longitude">
+        <br>
+  
+        <label for="img">Image</label>
+        <input type="text" name="img" v-model="apt.img">
+        <br>
+  
+        <div v-for="service in adds.additional_service" :key="service.id">
+          <input type="checkbox" :id="service.id" @change="getAdServices(service.id)" :checked="checkCheckboxes(apt, service)">
+          <label :for="service.name"> {{ service.name }} </label>
+        </div>
+  
+        <br>
+  
+        <button type="button" @click="updateData(apt)">Update Apartment</button>
+      </form>
     </section>
     <section v-else>
-        <h1 class="text-danger">NON AUTENTICATO</h1>
+      <h1 class="text-danger">NON AUTENTICATO</h1>
     </section>
-
-
-</template>
+  </template>
+  
 
 <style scoped></style>
