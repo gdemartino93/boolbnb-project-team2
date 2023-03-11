ff4fe3b6fc082ffcdf6db3baa163ad63b15bc502
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-11 15:55:42 +0100

    created component map

--- vue-frontend/src/components/Map/Map.vue
+++ vue-frontend/src/components/Map/Map.vue
@@ -1,53 +1,61 @@
 <template>
-    
-  <h1>Vue 3 TomTom Maps Demo</h1>
-  <div id='map' ref="mapRef"></div>
+  <div class="" style="position: sticky; top: 200px;">
+      <div id="map" style='width: 100%; height: 600px; border-radius: 10px;'></div>
+  </div>
 </template>
 
 <script>
-import { onMounted,  ref } from 'vue'
+import tt from "@tomtom-international/web-sdk-maps";
 export default {
-name: 'Map',
-setup() {
-  const mapRef = ref(null);
-  onMounted(() => {
-    
-          const tt = window.tt;
-          var map = tt.map({
-          key: 'BkGWT9JoOHyfQlGCvB9M8qbY5ZoUdNZo',
-          container: mapRef.value,
-          style: 'tomtom://vector/1/basic-main',
-      });
-      map.addControl(new tt.FullscreenControl());
-      map.addControl(new tt.NavigationControl()); 
-      addMarker(map);
-  })
-  function addMarker(map){
-     const tt = window.tt;
-      var location = [-121.91595, 37.36729];
-      var popupOffsets = {
-top: [0, 0],
-bottom: [0, -30],
-'bottom-right': [0, -30],
-'bottom-left': [0, -30],
-left: [25, -35],
-right: [-25, -35]
-}
-      var marker = new tt.Marker().setLngLat(location).addTo(map);
-      var popup = new tt.Popup({offset: popupOffsets}).setHTML("Your address!");
-marker.setPopup(popup).togglePopup();
-  }
-  return {
-    mapRef,
-  }
-  
-}
-
-}
+  name: "Map",
+  data() {
+      return {
+          map: null,
+          markers: [],
+      };
+  },
+  props: {
+      center: Object,
+      flats: Array,
+  },
+  watch: {
+      center(newValue) {
+          this.map.flyTo({ center: newValue, zoom: 10 });
+      },
+      flats(newValue) {
+          for (let marker of this.markers) {
+              marker.remove();
+          }
+          if (newValue) {
+              for (let flat of newValue) {
+                  this.markers.push(
+                      new tt.Marker()
+                          .setLngLat({ lon: flat.lon, lat: flat.lat })
+                          .addTo(this.map)
+                  );
+              }
+          }
+      },
+  },
+  mounted() {
+      this.startMap();
+  },
+  methods: {
+      startMap() {
+          this.map = tt.map({
+              key: "BkGWT9JoOHyfQlGCvB9M8qbY5ZoUdNZo",
+              container: "map",
+              center: this.center,
+              zoom: 10,
+              minZoom: 4,
+              // maxBounds: [
+              //     [1.7499552751, 1.619987291],
+              //     [19.4802470232, 48.1153931748],
+              // ],
+          });
+      },
+  },
+};
 </script>
-<style>
-#map {
-  height: 50vh;
-  width: 50vw;
-}
-</style>
+
+<style></style>
