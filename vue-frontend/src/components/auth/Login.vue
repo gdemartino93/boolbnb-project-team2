58194efab4e32fa276e436a5174b51c32c3eb139
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-14 19:44:48 +0100

    restored login

--- vue-frontend/src/components/auth/Login.vue
+++ vue-frontend/src/components/auth/Login.vue
@@ -1,42 +1,27 @@
 <template>
-
   <section class="container">
-    <div class="col-12 col-md-8 col-lg-6 mx-auto">
-      <span v-if="authStore.errors.email" >
+    <div class="col-12">
+      <span v-if="authStore.errors.email">
         <span class="text-danger">
-          {{ (authStore.errors.email[0]) === 'These credentials do not match our records.' 
-        ? 
-        "Nessuna corrispondenza "
-        : ""
-         }}</span>
+          {{ (authStore.errors.email[0]) === 'These credentials do not match our records.'
+            ?
+            "Nessuna corrispondenza "
+            : ""
+          }}</span>
       </span>
       <v-form @submit.prevent="authStore.handleLogin(form)">
-        <v-text-field
-                  append-icon="mdi-at"
-                  v-model="form.email"
-                  :rules="emailRules"
-                  label="E-mail"
-                  required
-        >
+        <v-text-field v-model="form.email" :rules="emailRules" label="E-mail" required>
         </v-text-field>
-        <v-text-field
-              v-model="form.password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              name="input-10-1"
-              label="Password"
-              counter
-              @click:append="show1 = !show1"
-          ></v-text-field>
+        <v-text-field v-model="form.password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password"
+          counter @click:append="show1 = !show1"></v-text-field>
         <!-- da inserire dopo aver validato nel backend la lunghezza minima della password  -->
-          <!-- hint="At least 8 characters" -->
+        <!-- hint="At least 8 characters" -->
 
         <v-btn type="submit" block class="mt-2">Submit</v-btn>
       </v-form>
     </div>
   </section>
-
 </template>
 
 <script>
@@ -51,24 +36,21 @@ export default {
         password: ""
       }),
       emailRules: [
-      value => {
-        if (value) return true
-
-        return 'E-mail is requred.'
-      },
-      value => {
-        if (/.+@.+\..+/.test(value)) return true
-
-        return 'E-mail must be valid.'
-      },
-    ],
-    show1: false,
+        value => {
+          if (value) return true
+          return 'E-mail is requred.'
+        },
+        value => {
+          if (/.+@.+\..+/.test(value)) return true
+          return 'E-mail must be valid.'
+        },
+      ],
+      show1: false,
       show2: true,
       password: 'Password',
       rules: {
         required: value => !!value || 'Required.',
         // da validare nel backend e poi scommentare
-
         // min: v => v.length >= 8 || 'Min 8 characters',
         // emailMatch: () => (`The email and password you entered don't match`),
       },
@@ -80,4 +62,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  margin-top: 150px;
+}
 </style>
