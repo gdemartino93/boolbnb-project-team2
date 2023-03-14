ce5c6e1a6c3b400706c87dce89e75de7691dd7c1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-14 20:38:08 +0100

    working on home

--- vue-frontend/src/components/AptCard.vue
+++ vue-frontend/src/components/AptCard.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="card" style="width: 18rem;">
-  <img :src="apartment.img" class="card-img-top" :alt="apartment.img">
-  <div class="card-body">
-    <!-- creiamo funzione per capitalizzare -->
-    <span class="fw-bold">{{ (apartment.title).charAt(0).toUpperCase() + apartment.title.slice(1) }}</span>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <span @click="toggleShow">Mostra v</span>
-    <ul>
-      <li v-if="isShow" v-for="additional_services in apartment.additional_services">
-                    {{ additional_services.name }}
-      </li>
-    </ul>
+    <img :src="apartment.img" class="card-img-top" :alt="apartment.img">
+    <div class="card-body">
+      <span class="fw-bold">{{ (apartment.title).charAt(0).toUpperCase() + apartment.title.slice(1) }}</span>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <span @click="toggleShow">{{ isShow ? 'Nascondi' : 'Mostra' }} v</span>
+      <ul>
+        <li v-for="(additional_service, index) in apartment.additional_services" :key="index" v-if="showIndex === index || isShow">
+          {{ additional_service.name }}
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
 </template>
-  <script>
-  export default {
-    props: ['apartment'],
-    data(){
-      return{
-        isShow : false,
-      }
+
+<script>
+export default {
+  props: ['apartment'],
+  data() {
+    return {
+      isShow: false,
+      showIndex: null
+    }
+  },
+  methods: {
+    toggleShow() {
+      this.isShow = !this.isShow;
     },
-    methods:{
-      toggleShow(){
-        this.isShow = !this.isShow
-      }
+    toggleIndex(index) {
+      this.showIndex = this.showIndex === index ? null : index;
     }
   }
+}
 </script>
+
 <style>
-ul{
-    list-style: none;
+ul {
+  list-style: none;
 }
 </style>
