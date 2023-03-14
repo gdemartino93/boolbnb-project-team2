0df4d0b273025a163f2796644a8f17eb418d2a21
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-14 17:00:30 +0100

    create new card component

--- vue-frontend/src/components/AptCard.vue
+++ vue-frontend/src/components/AptCard.vue
@@ -1,57 +1,56 @@
-<script>
-
-export default {
-    props: ['apartment'],
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-
-    },
-    mounted() {
-
-    }
-}
-</script>
-
 <template>
-
-        <router-link class="card col-4" :to="{name: 'show', params:{
-            id: `${apartment.id}`
-        }}" >
-
-<div>
-            <h5>
-                #{{ apartment.id }}
-            </h5>
-
-            <img :src="apartment.img" :alt="apartment.img">
-
-            <h3>Nome: {{ apartment.title }}</h3>
-
-            <h3>
-                Additional services: {{ apartment.additional_services.length }}
-            </h3>
+    <v-card
+      class="mx-auto"
+      max-width="344"
+    >
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px"
+        cover
+      ></v-img>
+  
+      <v-card-title>
+        Top western road trips
+      </v-card-title>
+  
+      <v-card-subtitle>
+        1,000 miles of wonder
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn
+          color="orange-lighten-2"
+          variant="text"
+        >
+          Explore
+        </v-btn>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn
+          :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          @click="show = !show"
+        ></v-btn>
+      </v-card-actions>
+  
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+  
+          <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
         </div>
-        
-    
-
-
-        </router-link>
-
-    <!-- </router-link> -->
-</template>
-
-<style lang="scss" scoped>
-.card{
-    border: 1px solid black;
-    border-radius: 10px;
-
-    img{
-        width: 50px;
-    }
-}
-
+      </v-expand-transition>
+    </v-card>
+  </template>
+  <script>
+  export default {
+    props: ['apartment'],
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
+<style>
 </style>
---
+++ vue-frontend/src/components/AptCardOLD.vue
@@ -0,0 +1,57 @@
+<script>
+
+export default {
+    props: ['apartment'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+
+        <router-link class="card col-4" :to="{name: 'show', params:{
+            id: `${apartment.id}`
+        }}" >
+
+<div>
+            <h5>
+                #{{ apartment.id }}
+            </h5>
+
+            <img :src="apartment.img" :alt="apartment.img">
+
+            <h3>Nome: {{ apartment.title }}</h3>
+
+            <h3>
+                Additional services: {{ apartment.additional_services.length }}
+            </h3>
+        </div>
+        
+    
+
+
+        </router-link>
+
+    <!-- </router-link> -->
+</template>
+
+<style lang="scss" scoped>
+.card{
+    border: 1px solid black;
+    border-radius: 10px;
+
+    img{
+        width: 50px;
+    }
+}
+
+</style>
