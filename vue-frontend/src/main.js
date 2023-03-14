3bf46a233b9def7362ae0581e08affa6c6bbe3f6
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-14 23:37:39 +0100

    imported spinner

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-
+// import VueSpinner from 'vue-spinner'
 // impostiamo interceptor di axios per far in modo che tutte le chiamate avvengono da questa base url 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:8000";
@@ -32,5 +32,6 @@ import './assets/main.css'
 
 app.use(pinia);
 app.use(router);
+// app.component('VueSpinner', VueSpinner)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app');
