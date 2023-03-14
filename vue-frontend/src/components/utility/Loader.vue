06d49399368e03000e3aa66ea4f92645d73d5937
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-14 23:38:01 +0100

    created component loader

---
+++ vue-frontend/src/components/utility/Loader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="loader">
+        <PulseLoader />
+    </div>
+</template>
+
+<script>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+
+    export default {
+        components : {
+            PulseLoader
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.loader{
+    width: 100vw;
+    height: 90vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+}
+</style>
