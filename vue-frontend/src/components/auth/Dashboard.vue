884ab02f18ceb3768fb5bd1e9213f54652becaad
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-16 14:02:38 +0100

    used modal from bs

--- vue-frontend/src/components/auth/Dashboard.vue
+++ vue-frontend/src/components/auth/Dashboard.vue
@@ -51,8 +51,32 @@ export default {
                         <button
                             class="edit-button btn btn-primary btn-block text-uppercase rounded-pill shadow-sm p-2">Modifica</button>
                     </router-link>
-                    <button
-                        class="visible-button btn btn-primary btn-block text-uppercase rounded-pill shadow-sm p-2">Occhio</button>
+                    <button class="visible-button btn btn-primary btn-block text-uppercase rounded-pill shadow-sm p-2">Occhio</button>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Launch static backdrop modal
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal il componente</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Understood</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
                 </li>
             </ul>
 
