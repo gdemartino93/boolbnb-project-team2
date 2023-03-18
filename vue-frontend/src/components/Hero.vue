1951a27429a3a4359aff21951ba7085ff8f60856
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-18 21:30:37 +0100

    added hero section

--- vue-frontend/src/components/Hero.vue
+++ vue-frontend/src/components/Hero.vue
@@ -6,42 +6,46 @@ export default{
 
 <template>
 
-    <div class="d-flex">
 
-        <section id="hero">
+    <section id="hero">
     
-        </section>
-    
-        <section>
-    
-            <router-link id="heroBtn" class="btn btn-block text-uppercase rounded-pill shadow-sm p-2 mt-5 mx-2"
+        <router-link id="heroBtn" class="btn btn-block text-uppercase rounded-pill shadow-sm p-2 mt-5 mx-2"
             :to="{ name: 'advancedSearch' }">Vai alla ricerca</router-link>
-        </section>
-    </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 
 #hero {
 
-    width: 50%;
-    height: 70vh;
-    background-image: url('../assets/xan-griffin-IM8AJKDavbk-unsplash.jpg');
+    width: 100%;
+    height: 50vh;
+    background-image: url('../assets/dream\ big..png');
     background-size: cover;
     background-repeat: no-repeat;
+    background-position: 50%;
+    position: relative;
 
-}
 
-#heroBtn{
-
-    border: 3px solid #FF6E31;
-    color: #FF6E31;
-    transition: .4s ease-in-out;
-
-    &:hover {
-
-        background-color: #FF6E31;
-        color: #fff;
+    #heroBtn{
+        
+        position: absolute;
+        top: 70%;
+        right: 10%;
+        border: 3px solid #ff3d00;
+        color: #ff3d00;
+        transition: .4s ease-in-out;
+    
+        &:hover {
+    
+            background-color: #ff3d00;
+            color: #fff;
+        }
     }
 }
+
+
+    
+
+
 </style>
