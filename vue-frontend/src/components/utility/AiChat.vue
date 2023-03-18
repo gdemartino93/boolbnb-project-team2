716a24644419f092e602624ae775cc4d2d527d40
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-18 02:37:03 +0100

    created component

--- vue-frontend/src/components/utility/AiChat.vue
+++ vue-frontend/src/components/utility/AiChat.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="box col-4">
-      <form action="" method="post" @submit.prevent="getResponse">
+      <form action="" method="post" @submit.prevent="getReply">
         <input type="text" v-model="message">
         <span>{{ risposta }}</span>
-        <button class="btn btn-success">Invia</button>
+        <button class="btn btn-success" type="submit">Invia</button>
       </form>
     </div>
   </template>
@@ -21,11 +21,19 @@
       };
     },
     methods: {
+      async getReply(){
+        try {
+          let response = await axios.post('/api/v1/chat', {message: this.message})
+          this.risposta = response.data.result
+        
+        } catch (error) {
+          console.log(error);
+        }
+      }
 
 
     }
 }
-  
   </script>
   
   <style lang="scss" scoped>
