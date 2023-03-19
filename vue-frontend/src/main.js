9daf1f8f05eb17176f5444b470db86bc0bc3ff65
19nikilangiu91 <niki.langiu@gmail.com>
2023-03-19 16:16:19 +0100

    “update-showcard”

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -9,25 +9,25 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:8000";
 
 const pinia = createPinia(); // crea l'istanza di Pinia prima di definire l'istanza dell'app Vue.js
-pinia.use(({ store}) =>{ //estendiamo lo store pinia per gestire il routing dell'applicazione. Quindi abbiamo accesso al router nel nostro store pinia.
-    store.router = markRaw(router)
+pinia.use(({ store }) => { //estendiamo lo store pinia per gestire il routing dell'applicazione. Quindi abbiamo accesso al router nel nostro store pinia.
+  store.router = markRaw(router)
 });
 const app = createApp(App);
 
-  /* import the fontawesome core */
+/* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faBars, faHome, faUser, faUserSecret, faFilter, faBed, faCheck, faEye } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faBed, faBath, faRuler, faLocationDot, faBars, faUser, faUserSecret, faFilter, faCheck, faEye } from '@fortawesome/free-solid-svg-icons'
 
 // import { faBars, faHome, faUser, faUserSecret, faPlus, faBed, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 
 /* add icons to the library */
-library.add(faUserSecret,faUser,faBars,faHome, faFilter, faBed, faCheck,faEye)
+library.add(faHome, faBed, faBath, faRuler, faLocationDot, faBars, faUser, faUserSecret, faFilter, faCheck, faEye)
 
 
 import 'bootstrap/dist/css/bootstrap.css'
