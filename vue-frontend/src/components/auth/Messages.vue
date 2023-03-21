d5c09eb071e668aa65824caf1badc3b01df8c2cb
19nikilangiu91 <niki.langiu@gmail.com>
2023-03-21 11:37:35 +0100

    “fix-message”

--- vue-frontend/src/components/auth/Messages.vue
+++ vue-frontend/src/components/auth/Messages.vue
@@ -1,86 +1,85 @@
 <script>
-export default{
+export default {
     props: ['apartment']
 }
 </script>
 
 <template>
-
-    <div class="ms_card">
+    <div class="ms_card col-12 mx-auto">
 
         <div class="pic">
-
+            <img :src="apartment.img" :alt="apartment.img">
         </div>
 
         <section class="ms_messagebox">
 
             <!-- Button trigger modal -->
-            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal" v-for="message in apartment.messages">
-                
-                <font-awesome-icon icon="fa-solid fa-envelope" class="letterIcon"/>
-                {{ message.mail }}
+            <button type="button" class="btn col-12 d-flex justify-content-evenly" data-bs-toggle="modal"
+                data-bs-target="#exampleModal" v-for="message in apartment.messages">
+                <div>
+                    <font-awesome-icon icon="fa-solid fa-envelope" class="letterIcon px-1" />
+                </div>
+                <div>
+                    {{ message.mail }}
+                </div>
             </button>
             <!-- Modal -->
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" v-for="message in apartment.messages">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title fs-5" id="exampleModalLabel">Il tuo messaggio da 
-                        <span style="color: #ff3d00">
+                <div class="modal-dialog" v-for="message in apartment.messages">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title fs-5" id="exampleModalLabel">Il tuo messaggio da
+                                <span style="color: #ff3d00">
 
-                            {{ message.name }}
-                        </span>
-                    </h3>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    {{message.text}}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                    <button type="button" class="btn btn-primary">Rispondi</button>
-                </div>
+                                    {{ message.name }}
+                                </span>
+                            </h3>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            {{ message.text }}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                            <button type="button" class="btn btn-primary">Rispondi</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-
         </section>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.ms_card {
+    width: 200px;
+    height: 350px;
 
-    .ms_card {
+    .pic {
+        width: 100%;
+        height: 50%;
+        background-size: contain;
+        background-repeat: no-repeat;
+        border-radius: 15px 15px 0 0;
 
-        width: 200px;
-        height: 300px;
-
-        .pic {
-
-            // immagine statica, sostituire con apartment.img
-            background-image: url('../../assets/webaliser-_TPTXZd9mOo-unsplash.jpg');
+        img {
             width: 100%;
-            height: 50%;
-            background-size: contain;
-            background-repeat: no-repeat;
-            border-radius: 15px 15px 0 0;
+            height: 100%;
         }
+    }
 
-        .ms_messagebox {
+    .ms_messagebox {
+        height: 25%;
+        overflow-y: auto;
 
-            height: max-content;
-            overflow-y: auto;
+        .btn {
+            background-image: linear-gradient(to right, #ff3d00, #FF5F00);
+            color: #fff;
 
-            .btn {
-                
-                background-image: linear-gradient(to right, #ff3d00, #FF5F00);
-                color: #fff;
-
-                .letterIcon {
-
-                    font-size: 13px;
-                }
+            .letterIcon {
+                font-size: 13px;
             }
         }
     }
+}
 </style>
