905f734cf20b978c7d2a9916ed01a8f116c0aa42
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-21 22:23:18 +0100

    store form it

--- vue-frontend/src/components/auth/AddApt.vue
+++ vue-frontend/src/components/auth/AddApt.vue
@@ -78,37 +78,37 @@ export default {
     <section v-if="auth.user" class="my-container">
         <form method="POST" class="d-flex flex-wrap justify-content-around bg-white rounded-2 p-5">
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="title">Title</label>
+                <label for="title">Nome</label>
                 <input type="text" name="title" v-model="form.title">
             </div>
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="description">Description</label>
+                <label for="description">Descrizione</label>
                 <input type="text" name="description" v-model="form.description">
             </div>
 
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="room_number">Room Number</label>
+                <label for="room_number">Numero di stanze</label>
                 <input type="number" name="room_number" v-model="form.room_number">
             </div>
 
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="bed_number">Bed Number</label>
+                <label for="bed_number">Numero di letti</label>
                 <input type="number" name="bed_number" v-model="form.bed_number">
             </div>
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="bath_number">Bath Number</label>
+                <label for="bath_number">Numero di bagni</label>
                 <input type="number" name="bath_number" v-model="form.bath_number">
             </div>
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="square_meters">Square Meters</label>
+                <label for="square_meters">m<sup>2</sup></label>
                 <input type="number" name="square_meters" v-model="form.square_meters">
             </div>
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="address">Address</label>
+                <label for="address">Indirizzo</label>
                 <input type="text" name="address" v-model="form.address">
             </div>
             <div class="col-sm-11 col-md-5 col-lg-5">
-                <label for="img">Image</label>
+                <label for="img">Foto</label>
                 <input type="file" name="img" @change="onFileChange">
             </div>
             <div class="d-flex flex-wrap ms-services">
