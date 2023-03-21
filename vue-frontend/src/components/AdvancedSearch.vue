ab36e4d4c4adc76a177bdb0dd5aa06963d34074a
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-21 21:00:12 +0100

    added catchphrase

--- vue-frontend/src/components/AdvancedSearch.vue
+++ vue-frontend/src/components/AdvancedSearch.vue
@@ -19,6 +19,7 @@ export default {
             room_number: '',
             bed_number: '',
             isOpen: false,
+            isFilled: false
         }
     },
     methods: {
@@ -40,6 +41,7 @@ export default {
             console.log(this.queryLatitude, this.queryLongitude);
 
             this.getApt();
+            this.isFilled = true;
         },
         getApt(){
 
@@ -112,6 +114,8 @@ export default {
             </form>
         </div>
 
+        <img v-if="isFilled" id="phrase" src="../assets/dreams-here.png" alt="">
+
         <div class="slider container d-flex gap-2">
 
             <AptCard class="searchCard my-4 col-12 col-sm-6 col-md-4 col-lg-3" v-for="(apartment, index) in apartments"
@@ -129,10 +133,18 @@ export default {
     background-image: url('../assets/bg.png');
     position: relative;
 
+    #phrase {
+
+        width: 20%;
+        position: absolute;
+        left: 45%;
+        top: 9%;
+    }
+
     .slider {
 
         position: absolute;
-        bottom: 10%;
+        bottom: 5%;
         width: 1000px;
         overflow-x: auto;
 
