8049a646ff84be79a7c51c60750d54e8fedd689a
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-03-21 20:41:53 +0100

    more disrespectful layout

--- vue-frontend/src/components/auth/Inbox.vue
+++ vue-frontend/src/components/auth/Inbox.vue
@@ -40,33 +40,51 @@ export default {
 <template>
     <div id="inboxContainer" class="container">
 
-        <div id="box" class="container gap-5 bg-white p-2">
-            <div id="header" class="text-white col-8 mx-auto fs-6 fw-bold p-1">Messaggi in arrivo</div>
-            <ul class="col-8 mx-auto">
-                <li v-for="(apartment, index) in authStore.aptMsg" :key="index" class="listItem col-11"
+        <div id="box" class="container gap-5 p-2">
+            <div class="header text-white col-8 mx-auto fs-6 fw-bold p-1">Messaggi in arrivo</div>
+            <div id="msgBox" class="col-8 mx-auto bg-white">
+                <div v-for="(apartment, index) in authStore.aptMsg" :key="index" class="listItem col-11"
                 @click="openMsg(apartment.id)">
-                    <div v-for="(message, id) in apartment.messages" :key="id"
-                        class="message d-flex align-items-center">
-                        {{ apartment.id }}
-                        <font-awesome-icon class="letter ms-2" icon="fa-solid fa-envelope" />
-                        <div class="ms-2"><b>{{ message.mail }}</b></div>
-                        <div class="date ms-auto">{{ message.created_at.slice(0, 16).replace('T', ' ') }}</div>
-                    </div>
-                </li>
-                
-                <div id="messageContent" v-if="isOpen">
+                    <p>Appartamento: <span style="color: #ff3d00">{{ apartment.title }}</span></p>
+                    <ul>
 
-                    <!-- <h3>Il tuo messaggio da {{ apartment.messages[1].name }}</h3>  -->
-                    <div v-for="(message, index) in apartment.messages" :key="index">
-                        <h3>Il tuo messaggio da {{ message.name }}</h3>
+                        <li v-if="apartment.messages.length > 0" v-for="(message, id) in apartment.messages" :key="id"
+                            class="message d-flex align-items-center">
+                            <font-awesome-icon class="letter ms-2" icon="fa-solid fa-envelope" />
+                            <p class="mail ms-2"><b>{{ message.mail }}</b></p>
+                            <p class="date ms-auto">{{ message.created_at.slice(0, 16).replace('T', ' ') }}</p>
+                        </li>
 
-                        <div>
-                            {{ message.text }}
-                        </div>
-                    </div>
+                        <li v-else>
+                            <p> Non hai messaggi per questo appartamento!</p>
+                        </li>
+                    </ul>
                 </div>
                 
-            </ul>
+            </div>
+                
+            <div id="messageContent" v-if="isOpen" class="col-8 mx-auto p-1">
+
+            
+                <!-- <h3>Il tuo messaggio da {{ apartment.messages[1].name }}</h3>  -->
+            
+                <div v-for="(message, index) in apartment.messages" :key="index">
+            
+                    <div class="header bot col-10 mx-auto fs-6 fw-bold p-1">Il tuo messaggio da <span style="color: #ff3d00">{{ message.name }}</span></div>
+
+            
+                    <div class="bg-white col-10 mx-auto mb-2 p-1">
+            
+                        {{ message.text }}
+            
+                        <button class="btn rounded-pill fw-bold">Rispondi</button>
+                    </div>
+
+            
+                </div>
+            
+            </div>
+                
         </div>
     </div>
 </template>
@@ -83,32 +101,80 @@ export default {
         margin-top: 5%;
     }
 
-    #header {
+    .header {
 
         background-color: #ff3d00;
     }
 
-    ul {
-        list-style-type: none;
+    #msgBox {
         border-left: 1px solid #2E3840;
         border-right: 1px solid #2E3840;
         border-bottom: 1px solid #2E3840;
-        height: 90%;
+        height: 50%;
+        overflow-y: auto;
 
         .listItem {
 
             cursor: pointer;
+            padding: 1rem;
 
-            .letter {
-                color: #ff3d00;
+            ul {
+                list-style-type: none;
+                .message {
+
+                    border: 1px solid #2E3840;
+                    border-radius: 20px;
+                    padding: .2rem .5rem;
+                    margin-bottom: .5rem;
+
+                    .mail {
+
+                        margin-top: 10px;
+                    }
+
+                    .letter {
+                        color: #ff3d00;
+                        font-size: 20px;
+                    }
+            
+                    .date {
+            
+                        font-size: 10px;
+                        margin-top: 10px;
+                    }
+                }
+
+
             }
-    
-            .date {
-    
-                font-size: 10px;
-            }
+
         }
 
+    }
+
+    #messageContent {
+
+        background-color: #ff3d00;
+        height: 50%;
+        overflow-y: auto;
+
+        .bot {
+
+            background-color: #fff;
+            color: #2E3840;
+        }
+
+        .btn {
+
+            border: 2px solid #ff3d00;
+            color: #ff3d00;
+            transition: .4s ease-in-out;
+
+            &:hover {
+
+                background-color: #ff3d00;
+                color: #fff;
+            }
+        }
     }
 }
 
